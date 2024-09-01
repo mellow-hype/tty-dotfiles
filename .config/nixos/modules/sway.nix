94579636edb7ed104dc230f4492a6721f4d2ce63
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2024-09-01 16:25:48 -0700

    simplify sway/nvidia setup with variables

--- .config/nixos/modules/sway.nix
+++ .config/nixos/modules/sway.nix
@@ -1,8 +1,10 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, variables, ... }:
 
-let
-  username = "hyper";
-in {
+{
+    # set the video driver to use (in case nvidia doesn't want to play nice with sway)
+    services.xserver.videoDrivers = variables.videoDrivers;
+
+    # set up greetd + tuigreet
     services.greetd = {
         enable = true;
         settings = {
@@ -12,7 +14,7 @@ in {
                 --asterisks \
                 --remember \
                 --greeting "welcome back" \
-                --cmd 'sway --unsupported-gpu'
+                --cmd 'sway'
           '';
         };
     };
@@ -43,6 +45,7 @@ in {
         moc
     ];
 
+    # enable sway and set up the environment a bit
     programs.sway = {
         enable = true;
         wrapperFeatures.gtk = true;
@@ -54,26 +57,28 @@ in {
         '';
     };
 
+    environment.sessionVariables = {
+        XDG_CURRENT_DESKTOP = "sway";
+    };
+
+    # systemd user services for kanshi
     systemd.user.services.kanshi = {
         description = "kanshi daemon";
         serviceConfig = {
             Type = "simple";
-            ExecStart = ''${pkgs.kanshi}/bin/kanshi -c /home/${username}/.config/kanshi/config'';
+            ExecStart = ''${pkgs.kanshi}/bin/kanshi -c /home/${variables.username}/.config/kanshi/config'';
         };
     };
 
+    # systemd user services for gammastep
     systemd.user.services.gammastep = {
         description = "gammastep daemon";
         serviceConfig = {
             Type = "simple";
             ExecStart = ''
-                ${pkgs.gammastep}/bin/gammastep -c /home/${username}/.config/gammastep/config.ini
+                ${pkgs.gammastep}/bin/gammastep -c /home/${variables.username}/.config/gammastep/config.ini
             '';
         };
-    };
-
-    environment.sessionVariables = {
-        XDG_CURRENT_DESKTOP = "sway";
     };
 
     # Enable Polkit
