c43c2d111a66e2071830e1ae22f6327a843ee201
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2024-09-02 00:01:47 -0700

    remove scripts, tracked separately

--- bin/pipewire/now-playing.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# Super simple "Now Playing" util to get the media.name prop of all Pipewire audio output streams
-
-pw-dump | jq '.[].info.props | select(."media.class" == "Stream/Output/Audio") | {"media.name"} | join("")'
--- bin/pipewire/pw-streams-json.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-pw-dump | jq '.[].info.props | select(."media.class" == "Stream/Output/Audio") | {"application.name", "media.name", "client.id", "object.id"}'
