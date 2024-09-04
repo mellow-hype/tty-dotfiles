d58244066e1642ac5850490206807a07deae1ec5
mellow-hype <25216562+mellow-hype@users.noreply.github.com>
2024-09-04 00:05:38 -0700

    move nvidia+sway settings to move appropriate locations

--- .config/nixos/modules/nvidia.nix
+++ .config/nixos/modules/nvidia.nix
@@ -1,26 +1,35 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, variables, ... }:
 
 {
+    # Set this var to true to let other config files know
+    variables.useNvidia = true;
+
+    # set videoDrivers for GUI
+    services.xserver.videoDrivers = ["nvidia"];
+
     # enable OpenGL
-    hardware.opengl.enable = true;
+    hardware.opengl = {
+        enable = true;
+        driSupport = true;
+        extraPackages = with pkgs; [
+            vulkan-tools
+            vulkan-validation-layers
+            mesa
+        ];
+    };
 
     # load modules explicitly
     boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_drm" ];
 
-  # NVIDIA setup
-  hardware.nvidia = {
-      modesetting.enable = true;
-      powerManagement.enable = false;
-      powerManagement.finegrained = false;
-      open = false;
-      nvidiaPersistenced = true;
-      nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  programs.sway.extraSessionCommands = ''
-      export WLR_DRM_DEVICES="/dev/dri/card1:/dev/dri/card0"
-      export WLR_RENDERER=vulkan
-  '';
+    # NVIDIA setup
+    hardware.nvidia = {
+        modesetting.enable = true;
+        powerManagement.enable = false;
+        powerManagement.finegrained = false;
+        open = false;
+        nvidiaPersistenced = true;
+        nvidiaSettings = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
 
 }
--- .config/nixos/modules/sway.nix
+++ .config/nixos/modules/sway.nix
@@ -1,12 +1,13 @@
 { config, pkgs, lib, variables, ... }:
 
+let
+    kanshi_config = "/home/${variables.username}/.config/kanshi/config";
+    gamma_config = "/home/${variables.username}/.config/gammastep/config.ini";
+    sway_cmd = "sway ${if variables.useNvidia then "--unsupported-gpu" else ""}";
+in
+
 {
     imports = [ /etc/nixos/modules/gui-common.nix ];
-
-    # set the video driver to use (in case nvidia doesn't want to play nice with sway)
-    services.xserver.videoDrivers = lib.mkIf variables.useNvidia [
-        "nvidia"
-    ];
 
     # set up greetd + tuigreet
     services.greetd = {
@@ -18,7 +19,7 @@
                 --asterisks \
                 --remember \
                 --greeting "welcome back" \
-                --cmd 'sway ${if variables.useNvidia then "--unsupported-gpu" else ""}'
+                --cmd '${sway_cmd}'
           '';
         };
     };
@@ -27,29 +28,27 @@
       sway
     '';
 
-    # install sway and other common gui packages
-    environment.systemPackages = with pkgs; [
-        swaylock
-        swayidle
-        wl-clipboard
-        grim
-        slurp
-        foot
-        kanshi
-        fuzzel
-        dmenu
-        dunst
-        dex
-        gammastep
-        i3status
-        feh
-        wdisplays
-    ];
-
     # enable sway and set up the environment a bit
     programs.sway = {
         enable = true;
         wrapperFeatures.gtk = true;
+        extraPackages = with pkgs; [
+            swaylock
+            swayidle
+            i3status
+            wl-clipboard
+            grim
+            slurp
+            foot
+            feh
+            kanshi
+            fuzzel
+            dmenu
+            dunst
+            dex
+            gammastep
+            wdisplays
+        ];
         extraSessionCommands = ''
           export MOZ_ENABLE_WAYLAND=1
           export _JAVA_AWT_WM_NONREPARENTING=1
@@ -59,6 +58,14 @@
           export WLR_DRM_DEVICES="/dev/dri/card1:/dev/dri/card0"
           export WLR_NO_HARDWARE_CURSORS=1
           export XWAYLAND_NO_GLAMOR=1
+          ${if variables.useNvidia then ''
+            export WLR_DRM_DEVICES="/dev/dri/card1:/dev/dri/card0"
+            export WLR_RENDERER=vulkan
+            export GBM_BACKEND=nvidia-drm
+            export __GL_GSYNC_ALLOWED=0
+            export __GL_VRR_ALLOWED=0
+            export __GLX_VENDOR_LIBRARY_NAME=nvidia
+          '' else ''''}
         '';
     };
 
@@ -71,7 +78,7 @@
         description = "kanshi daemon";
         serviceConfig = {
             Type = "simple";
-            ExecStart = ''${pkgs.kanshi}/bin/kanshi -c /home/${variables.username}/.config/kanshi/config'';
+            ExecStart = ''${pkgs.kanshi}/bin/kanshi -c ${kanshi_config}'';
         };
     };
 
@@ -81,7 +88,7 @@
         serviceConfig = {
             Type = "simple";
             ExecStart = ''
-                ${pkgs.gammastep}/bin/gammastep -c /home/${variables.username}/.config/gammastep/config.ini
+                ${pkgs.gammastep}/bin/gammastep -c ${gamma_config}
             '';
         };
     };
